97d9234557704935f8ccb8f70441821bf86e5c23
Zaupower <marcelomascfb22@gmail.com>
2022-09-21 17:02:25 +0100

    prj creation

---
+++ ConsoleAppCalculator/ConsoleAppCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int num1 = 0; int num2 = 0;
+
+// Ask the user to type the first number.
+Console.WriteLine("Enter coordinate x of dot A");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number.
+Console.WriteLine("Type another number, and then press Enter");
+num2 = Convert.ToInt32(Console.ReadLine());
