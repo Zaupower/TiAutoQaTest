3a9a4534ab8a7ad214140b79da4eb1ad5a9784d3
Zaupower <marcelomascfb22@gmail.com>
2022-09-21 17:27:34 +0100

    Calculator

---
+++ ConsoleAppCalculator/ConsoleAppCalculator/Calculator.cs
@@ -0,0 +1,36 @@
+namespace ConsoleAppCalculator;
+using System;
+
+public class Calculator
+{
+    //all sides equal and angles
+    public static bool IsEquilateral()
+    {
+        return false;
+    }
+    //two sides equal and two angles equal
+    public static bool IsIsosceles()
+    {
+        return false;
+    }
+    //one of the angles is 90º
+    public static bool IsRight()
+    {
+        return false;
+    }
+    //P = a + b + c
+    public static bool getPerimeter()
+    {
+        return false;
+    }
+    /*
+     * if ( i % 2 == 0 )
+  		{
+  			cout << i <<" ";
+		}	
+     */
+    public static bool printEvenNumbers()
+    {
+        return false;
+    }
+}
--- ConsoleAppCalculator/ConsoleAppCalculator/Program.cs
+++ ConsoleAppCalculator/ConsoleAppCalculator/Program.cs
@@ -1,13 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Notes there are coordinates because the triangle is being defined on 2d plan
+class  Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
-
-int num1 = 0; int num2 = 0;
+        int coordinateXA = 0; 
+        int coordinateYA = 0; 
+        int coordinateXB = 0; 
+        int coordinateYB = 0; 
+        int coordinateXC = 0; 
+        int coordinateYC = 0; 
 
 // Ask the user to type the first number.
-Console.WriteLine("Enter coordinate x of dot A");
-num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter coordinate x of dot A");
+        coordinateXA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter coordinate y of dot A");
+        coordinateYA = Convert.ToInt32(Console.ReadLine());
 
-// Ask the user to type the second number.
-Console.WriteLine("Type another number, and then press Enter");
-num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter coordinate x of dot B");
+        coordinateXB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter coordinate y of dot B");
+        coordinateYB = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter coordinate x of dot C");
+        coordinateXC = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter coordinate y of dot C");
+        coordinateYC = Convert.ToInt32(Console.ReadLine());
+    }
+    
+}
