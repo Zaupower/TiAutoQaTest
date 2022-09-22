e63a69e04d031872b87e9252baf08c6338b333ed
Zaupower <marcelomascfb22@gmail.com>
2022-09-22 10:31:24 +0100

    distance calculater enhanced

--- ConsoleAppCalculator/ConsoleAppCalculator/Program.cs
+++ ConsoleAppCalculator/ConsoleAppCalculator/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Notes there are coordinates because the triangle is being defined on 2d plan
+
+using ConsoleAppCalculator;
+
 class  Program
 {
     static void Main(string[] args)
@@ -7,17 +10,18 @@ class  Program
         
         Console.WriteLine("Hello, World!");
 
-        int coordinateXA = 0; 
-        int coordinateYA = 0; 
-        int coordinateXB = 0; 
-        int coordinateYB = 0; 
-        int coordinateXC = 0; 
-        int coordinateYC = 0; 
+        double coordinateXA = 15; 
+        double coordinateYA = 0; 
+        double coordinateXB = 0; 
+        double coordinateYB = 0; 
+        double coordinateXC = 7.5; 
+        double coordinateYC = 0; 
 
         double ab = 0.0;
         double bc = 0.0;
         double ca = 0.0;
 
+        /*
         // Ask the user to type the first number.
         Console.WriteLine("Enter coordinate x of dot A");
         coordinateXA = Convert.ToInt32(Console.ReadLine());
@@ -38,6 +42,20 @@ class  Program
 
         Console.WriteLine("Enter coordinate y of dot C");
         coordinateYC = Convert.ToInt32(Console.ReadLine());
+        */
+
+        TriangleCalculator Tc = new TriangleCalculator();
+       
+        ab = Tc.convertCoodinatesToDistance(coordinateXA, coordinateYA, coordinateXB, coordinateYB);
+        bc = Tc.convertCoodinatesToDistance(coordinateXB, coordinateYB, coordinateXC, coordinateYC);
+        ca = Tc.convertCoodinatesToDistance(coordinateXC, coordinateYC, coordinateXA, coordinateYA);
+        Math.Round(ab, 2);
+        Math.Round(bc, 2);
+        Math.Round(ca, 2);
+        
+        Console.WriteLine("ab: "+ab);
+        Console.WriteLine("bc: "+ab);
+        Console.WriteLine("ca: "+ab);
     }
     
 }
